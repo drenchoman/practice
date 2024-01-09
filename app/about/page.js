ef622202d2fd96dccf00f8591379b7b6258c1902
Oscar Harron <oscarharron@hotmail.com>
2024-01-09 21:16:40 +1300

    add routing

---
+++ app/about/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <main>
+      <h1>About us</h1>
+      <p>
+        <Link href="/">Home</Link>
+      </p>
+    </main>
+  );
+}
